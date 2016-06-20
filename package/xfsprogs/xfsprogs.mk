c101f7f15a6dc4a9f69a1c5a2d663247dc0854da
xlcwu <xlcwu.taiwan@gmail.com>
2016-06-20 03:16:00 +0000

    xfsprogs: bump version to 4.5.0 and variants patch
    xfsprogs build shared library and workaroud parallel install. remove cross
    compile patch 0003 already included in "xfsprogs-4.3.0-cross-compile.patch".
    The musl patch by "xfsprogs-4.3.0-musl.patch". and all the patches from gentoo
    proj/musl.git https://gitweb.gentoo.org/proj/musl.git/tree/sys-fs/xfsprogs

--- package/xfsprogs/xfsprogs.mk
+++ package/xfsprogs/xfsprogs.mk
@@ -5,10 +5,27 @@
 ################################################################################
 
 XFSPROGS_VERSION = 4.11.0
+XFSPROGS_VERSION = 4.5.0
 XFSPROGS_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/fs/xfs/xfsprogs
 XFSPROGS_SOURCE = xfsprogs-$(XFSPROGS_VERSION).tar.xz
 
 XFSPROGS_DEPENDENCIES = util-linux
+
+# The libdir variable in lib*.la is empty, so let's fix it. This is
+# probably due to acl not using automake, and not doing fully the
+# right thing with libtool.
+define XFSPROGS_FIX_LIBTOOL_LA_LIBDIR
+	$(SED) "s,libdir=.*,libdir='$(STAGING_DIR)/usr/lib'," \
+		$(STAGING_DIR)/usr/lib/libhandle.la
+	$(SED) "s,libdir=.*,libdir='$(STAGING_DIR)/usr/lib'," \
+		$(STAGING_DIR)/usr/lib/libxcmd.la
+	$(SED) "s,libdir=.*,libdir='$(STAGING_DIR)/usr/lib'," \
+		$(STAGING_DIR)/usr/lib/libxfs.la
+	$(SED) "s,libdir=.*,libdir='$(STAGING_DIR)/usr/lib'," \
+		$(STAGING_DIR)/usr/lib/libxlog.la
+endef
+
+XFSPROGS_POST_INSTALL_STAGING_HOOKS += XFSPROGS_FIX_LIBTOOL_LA_LIBDIR
 
 XFSPROGS_CONF_ENV = ac_cv_header_aio_h=yes ac_cv_lib_rt_lio_listio=yes
 XFSPROGS_CONF_OPTS = \
@@ -18,6 +35,21 @@ XFSPROGS_CONF_OPTS = \
 	INSTALL_GROUP=root \
 	--enable-static
 
-XFSPROGS_INSTALL_TARGET_OPTS = DIST_ROOT=$(TARGET_DIR) install
+ifeq ($(BR2_STATIC_LIBS),y)
+XFSPROGS_CONF_OPTS += --disable-shared --enable-static
+else ifeq ($(BR2_SHARED_LIBS),y)
+XFSPROGS_CONF_OPTS += --enable-shared --disable-static
+
+XFSPROGS_POST_CONFIGURE_HOOKS += XFSPROGS_ENABLE_SHARED
+
+define XFSPROGS_ENABLE_SHARED
+	find $(@D) -name Makefile -exec \
+		sed -i -r -e '/^LLDFLAGS [+]?= -static(-libtool-libs)?$$/d' {} +
+endef
+endif
+
+XFSPROGS_INSTALL_TARGET_OPTS += -j1 DIST_ROOT=$(TARGET_DIR) install install-dev
+XFSPROGS_INSTALL_STAGING = YES
+XFSPROGS_INSTALL_STAGING_OPTS += -j1 DIST_ROOT=$(STAGING_DIR) install install-dev libxfs-install-dev
 
 $(eval $(autotools-package))
