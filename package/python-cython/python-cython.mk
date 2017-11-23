af13943862ad30d3822c660c9d833df1b8d3a45d
Joe Lin <xlcwu.taiwan@gmail.com>
2017-11-23 07:02:59 +0000

    python-cython: bump version to 0.24.1

--- package/python-cython/python-cython.mk
+++ package/python-cython/python-cython.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_CYTHON_VERSION = 0.19.2
+PYTHON_CYTHON_VERSION = 0.24.1
 PYTHON_CYTHON_SOURCE = Cython-$(PYTHON_CYTHON_VERSION).tar.gz
 PYTHON_CYTHON_SITE = http://cython.org/release
-PYTHON_CYTHON_SETUP_TYPE = distutils
+PYTHON_CYTHON_SETUP_TYPE = setuptools
 PYTHON_CYTHON_LICENSE = Apache-2.0
 PYTHON_CYTHON_LICENSE_FILES = LICENSE.txt
 
